e5634c0f7bdfe54fef18ecec6a7aa0ad43e6938a
Chris Watson <cawatson1993@gmail.com>
2020-04-01 23:39:42 -0600

    Fixed ban

--- userbot/modules/admin/ban.py
+++ userbot/modules/admin/ban.py
@@ -12,9 +12,30 @@ class BanCommand(Command):
     category = "admin"
 
     async def exec(self, event):
+        await event.delete()
+
         dbchat = Chat.objects(chat_id=event.chat_id).get() # pylint: disable=no-member
         bancommand = dbchat.ban_command
-        reason = event.pattern_match.groups()[0] or ""
+        args, maybe_user = parse_arguments(event.pattern_match.group(1), [ 'user', 'reason' ])
+        parts = re.split(r'\s+', maybe_user, 1)
+        args['user'] = args.get('user') or parts[0]
+
+        if len(parts) > 1 and not args.get('reason'):
+            args['reason'] = parts[1]
+        else:
+            args['reason'] = ''
+
+        reason = args.get('reason')
+
+        try:
+            user_full = await get_user_from_event(event, **args)
+        except BaseException:
+            user_full = None
+
+        if not user_full:
+            await log_message("**Failed to get information for user**\n" \
+                              f"Command: `{event.message.message}`")
+            return
 
         if event.reply_to_msg_id:
             reply = event.reply_to_msg_id
@@ -22,5 +43,3 @@ class BanCommand(Command):
         else:
             user_full = await get_user_from_event(event)
             await event.respond(f"{bancommand} {user_full.user.id} {reason}")
-
-        await event.delete()
